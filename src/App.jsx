787e26614508c3474dc43128223951a4f60247ad
AlessandraSantanna <alesy2007@gmail.com>
2024-10-09 21:14:00 -0300

    twitter

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Sidebar } from "./componentes/Sidebar"
+
+function App() {
+ 
+
+  return (
+    
+      <div className= "flex mx-auto max-w-7xl">
+        
+        <Sidebar /> 
+
+       </div> 
+        
+    
+  )
+}
+
+export default App
---
+++ src/componentes/Sidebar/index.jsx
@@ -0,0 +1,11 @@
+import {FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faTwitter} from '@fortawesome/free-bands-svg-icons'
+
+export function Sidebar(){
+
+    return (
+        <div className="w-20 xl:w-64 sticky top-0 px-2 h-screen">
+             <FontAwesomeIcon icon={faTwitter} className='text-blue-400 text-3xl m-4'/>
+        </div>
+    )
+}
